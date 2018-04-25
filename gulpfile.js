52889432d84d594db2b1ea208dad754b6b077a99
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-04-25 09:03:09 +0200

    Updating configuration files

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp = require('gulp');
 const sass = require('node-sass');
 const inlineTemplates = require('gulp-inline-ng2-template');
 const exec = require('child_process').exec;
-const htmlMinifier = require("html-minifier");/*!!!!*/
 const copyfiles = require('copyfiles');
 const cssimport = require("gulp-cssimport");
 const replace = require('gulp-replace');
@@ -53,41 +52,3 @@ gulp.task('delete.angular.imports', function () {
     .pipe(replace(THEMES_STYLES_CONF.MATERIAL_IMPORT, ''))
     .pipe(gulp.dest(THEMES_STYLES_CONF.STYLES_TMP));
 });
-
-/**
- * Inline templates configuration.
- * @see  https://github.com/ludohenin/gulp-inline-ng2-template
- */
-const INLINE_TEMPLATES_CONF = {
-  SRC: ['./**/*.ts', '!./tmp/**/*', '!./node_modules/**/*', '!./custom-typings.d.ts'],
-  DIST: './tmp',
-  CONFIG: {
-    base: '.',
-    target: 'es6',
-    useRelativePaths: true,
-    styleProcessor: compileSass
-  }
-};
-
-/**
- * Inline external HTML and SCSS templates into Angular component files.
- * @see: https://github.com/ludohenin/gulp-inline-ng2-template
- */
-gulp.task('inline-templates', () => {
-  return gulp.src(INLINE_TEMPLATES_CONF.SRC)
-    .pipe(inlineTemplates(INLINE_TEMPLATES_CONF.CONFIG))
-    .pipe(gulp.dest(INLINE_TEMPLATES_CONF.DIST));
-});
-
-/**
- * Compile SASS to CSS.
- * @see https://github.com/ludohenin/gulp-inline-ng2-template
- * @see https://github.com/sass/node-sass
- */
-function compileSass(path, ext, file, callback) {
-  let compiledCss = sass.renderSync({
-    file: path,
-    outputStyle: 'compressed',
-  });
-  callback(null, compiledCss.css);
-}
