b05c6791040652864039466781f09092b2a8589a
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-04-25 09:03:33 +0200

    Deleting unused files

--- index.ts
+++ index.ts
@@ -1,20 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { OTHEMING_MODULES } from './src/components';
-// import { OTHEMING_PROVIDERS } from './src/services';
-
-/**
- * Exports
- */
-export * from './src/components';
-export * from './src/services';
 
 @NgModule({
-  imports: [
-    CommonModule
-    // ,
-    // ...OTHEMING_MODULES
-  ],
+  imports: [CommonModule]
   // exports: OTHEMING_MODULES,
   // providers: OTHEMING_PROVIDERS
 })
