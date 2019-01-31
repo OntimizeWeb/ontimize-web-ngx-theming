c59e06aa190ef95e4353ff5d35ccd94bfabad607
Patricia Martinez <patricia.martinez@imatia.com>
2019-01-31 08:37:10 +0100

    Removed unnecessary code

--- index.ts
+++ index.ts
@@ -3,7 +3,5 @@ import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [CommonModule]
-  // exports: OTHEMING_MODULES,
-  // providers: OTHEMING_PROVIDERS
 })
 export class OThemingModule { }
